d6ba6b1d4ef372cc345c64b8f95c718a6840ff31
JustPav <aksenovpaveldmitrievich@gmail.com>
2026-01-22 17:44:18 +0400

    Удалены остаточные упоминания файлов

--- upm-core/src/lib.rs
+++ upm-core/src/lib.rs
@@ -6,24 +6,18 @@ pub mod core {
     pub mod thread_coordinator;
 }
 
-pub mod dependency {
-    pub mod resolver;
-}
-
 pub mod types {
-    pub mod package;
-    pub mod operation;
     pub mod errors;
+    pub mod package;
 }
 
 pub use core::manager::PackageManager;
 pub use core::thread_coordinator::ThreadCoordinator;
-pub use types::{Package, PackageInfo, Operation, Result};
+pub use types::{Package, PackageInfo, Result};
 
 pub mod prelude {
     pub use crate::{
-        PackageManager,
-        ThreadCoordinator,
-        types::{Package, PackageInfo, Operation, Result},
+        types::{Operation, Package, PackageInfo, Result},
+        PackageManager, ThreadCoordinator,
     };
 }
